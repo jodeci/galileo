39fcbb2d2fe723eda8a3c022961687bb8698fd46
Tsehau Chao <jodeci@5xruby.tw>
2016-10-21 21:27:54 +0800

    view cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def dropdown_title(title)
+    content_tag :a, dropdown_title_hash do
+      concat title
+      concat content_tag :span, nil, class: "caret"
+    end
+  end
+
+  def dropdown_title_hash
+    {
+      "class": "dropdown-toggle",
+      "data-toggle": "dropdown",
+      "role": "button",
+      "aria-haspopup": "true",
+      "aria-expanded": "false"
+    }
+  end
 end
