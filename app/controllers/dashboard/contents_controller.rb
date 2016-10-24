c8b43cc5626aed8c82ccbee670f45f806d0fce0e
Tsehau Chao <jodeci@5xruby.tw>
2016-10-24 11:20:19 +0800

    add dashboard content links

--- app/controllers/dashboard/contents_controller.rb
+++ app/controllers/dashboard/contents_controller.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 class Dashboard::ContentsController < Dashboard::BaseController
+  private
+
+  def collection_scope
+    if params[:id]
+      Content
+    else
+      Content.order(id: :desc)
+    end
+  end
+
+  def resource_params
+    params.require(:contents).permit(
+      :title, :body, :abstract, :template, :cover_image, :status
+    )
+  end
 end
