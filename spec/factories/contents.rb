56b7517f4ab925e6299f45e92fde7b5643858d59
Tsehau Chao <jodeci@5xruby.tw>
2016-11-17 13:58:37 +0800

    add content test

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :content do
+    title { Faker::Book.title }
+    slug { Faker::Internet.slug }
   end
 end
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -2,4 +2,21 @@
 require "rails_helper"
 
 RSpec.describe Content, type: :model do
+  let(:content) { FactoryGirl.create(:content) }
+
+  describe "validations" do
+    context "for slug" do
+      subject { FactoryGirl.build(:content) }
+      it { should validate_uniqueness_of(:slug).with_message(I18n.t("validation.uniqueness")) }
+
+      context "when format is invalid" do
+        it { should_not allow_value("中文").for(:slug).with_message(I18n.t("validation.format.slug")) }
+        it { should_not allow_value("no spaces").for(:slug).with_message(I18n.t("validation.format.slug")) }
+      end
+
+      context "when format is valid" do
+        it { should allow_value("This-is-okay_123").for(:slug) }
+      end
+    end
+  end
 end
