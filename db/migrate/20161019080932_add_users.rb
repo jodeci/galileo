a113c900e54e2f0d065bda2c7a3a68d6e75509b8
Tsehau Chao <jodeci@5xruby.tw>
2018-08-17 13:26:50 +0800

    update ActiveRecord::Migration version

--- db/migrate/20161019080932_add_users.rb
+++ db/migrate/20161019080932_add_users.rb
@@ -1,4 +1,4 @@
-class AddUsers < ActiveRecord::Migration[5.0]
+class AddUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
     add_column :users, :username, :string, null: false
